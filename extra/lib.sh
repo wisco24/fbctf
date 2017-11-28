c54765bd6411192c2853bad9e51c1fa9d5a19f7e
Wisco24 <eshulze@gmail.com>
2017-11-28 17:10:23 -0600

    Missing HHVM Apt-Source - Commenting out since downloaded later.

--- extra/lib.sh
+++ extra/lib.sh
@@ -215,7 +215,7 @@ function install_hhvm() {
   sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0x5a16e7281be7a449
 
   log "Adding HHVM repo"
-  sudo add-apt-repository "deb http://dl.hhvm.com/ubuntu $(lsb_release -sc) main"
+  #sudo add-apt-repository "deb http://dl.hhvm.com/ubuntu $(lsb_release -sc) main"
 
   log "Installing HHVM"
   sudo apt-get update
